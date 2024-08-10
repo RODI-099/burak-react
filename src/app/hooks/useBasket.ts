f14680c0bb77f327293c403d70652dbf6856085f
RODI-099 <rodiiy099@gmail.com>
2024-08-10 22:17:11 +0500

    fix: modify useBasket

--- src/app/hooks/useBasket.ts
+++ src/app/hooks/useBasket.ts
@@ -1,50 +1,52 @@
 import { useState } from "react";
 import { CartItem } from "../../lib/types/search";
-
+import { idText } from "typescript";
 
 const useBasket = () => {
     const cartJson: string | null = localStorage.getItem("cartData");
     const currentCart = cartJson ? JSON.parse(cartJson) : [];
-    const [cartItems, setCartItems] = useState<CartItem[]>([currentCart]);
+    const [cartItems, setCartItems] = useState<CartItem[]>(currentCart);
 
     const onAdd = (input: CartItem) => {
-        const exist: any = cartItems.find((item: CartItem) => item._id === input._id);
-        if(exist) {
-          const cartUpdate = cartItems.map((item: CartItem) => 
-              item._id === input._id ? {
-              ...exist, quantity: exist.quantity + 1
-             } : item
-          );
-          setCartItems(cartUpdate);
-          localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    
-    
-        } else {
-          const cartUpdate = [ ...cartItems, {...input}];
-          setCartItems(cartUpdate);
-          localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    
-        }
-    
+        const exist:  any = cartItems.find((item:CartItem) => item._id === input._id
+      );
+      if(exist) {
+        const cartUpdate = cartItems.map((item: CartItem) => 
+        item._id === input._id ? { ...exist, quantity: exist.quantity + 1 } : item
+        );
+        setCartItems(cartUpdate);
+        localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+      } else {
+        const cartUpdate = [...cartItems, {...input}];
+        setCartItems(cartUpdate);
+        localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+      }
       };
 
       const onRemove = (input: CartItem) => {
         const exist: any = cartItems.find(
-            (item: CartItem) => item._id === item._id
+            (item: CartItem) => item._id === input._id
         );
-        if(exist.quantity ===1) {
-            const cartUpdate = cartItems.filter((item: CartItem) => item._id !== input._id );
+        if(exist.quantity === 1) {
+            const cartUpdate = cartItems.filter(
+                (item: CartItem) => item._id !== input._id
+            );
             setCartItems(cartUpdate);
-          localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+            localStorage.setItem("cartData", JSON.stringify(cartUpdate));
         } else {
-            const cartUpdate = cartItems.map((item: CartItem) => item._id === input._id ? {...exist, quantity: exist.quantity -1} : item);
-            setCartItems(cartUpdate);
-          localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+            const cartUpdate = cartItems.map((item: CartItem) => 
+            item._id === input._id 
+              ? { ...exist, quantity:exist.quantity - 1 } 
+              : item );
+              setCartItems(cartUpdate);
+            localStorage.setItem("cartData", JSON.stringify(cartUpdate));
         }
       };
 
       const onDelete = (input: CartItem) => {
-        const cartUpdate = cartItems.filter((item: CartItem) => item._id !== item._id);
+        const cartUpdate = cartItems.filter(
+            (item: CartItem) => item._id !== input._id
+        );
         setCartItems(cartUpdate);
         localStorage.setItem("cartData", JSON.stringify(cartUpdate));
       };
@@ -52,18 +54,15 @@ const useBasket = () => {
       const onDeleteAll = () => {
         setCartItems([]);
         localStorage.removeItem("cartData");
-
-      };
-
+      }
 
       return {
         cartItems,
         onAdd,
         onRemove,
         onDelete,
-        onDeleteAll
+        onDeleteAll,
       };
-
 };
 
 export default useBasket;
