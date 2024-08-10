ce1384430a186fd9e4781221e7b6d42bd8bee8b9
RODI-099 <rodiiy099@gmail.com>
2024-08-10 18:38:51 +0500

    fix: create login process

--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -9,7 +9,7 @@ import LoginIcon from "@mui/icons-material/Login";
 import { T } from "../../../lib/types/common";
 import { Message } from "@mui/icons-material";
 import { Messages } from "../../../lib/config";
-import { MemberInput } from "../../../lib/types/member";
+import { LoginInput, MemberInput } from "../../../lib/types/member";
 import MemberService from "../../services/MemberService";
 import { sweetErrorHandling } from "../../../lib/sweetAlert";
 
@@ -66,13 +66,15 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
    };
    const handlepasswordKewDown = (e: T) => {
     if(e.key === "Enter" && signupOpen) {
-      handleSignupRequest().then()
-    }
+      handleSignupRequest().then();
+     } else if(e.key === "Enter" && loginOpen) {
+       handleLoginRequest().then()
+
+     }
 
    }
    const handleSignupRequest = async () => {
     try {
-      console.log("inputs:", memberNick, memberPhone, memberPassword);
       const isFulfill = memberNick !== "" && memberPhone !== " " && memberPassword !== "";
       if(!isFulfill) throw new Error(Messages.error3);
       
@@ -84,11 +86,35 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
 
       const member = new MemberService();
       const result = await member.signup(signupInput);
+      // Saving Authenticated User
       handleSignupClose();
       
 
     } catch(err) {
       console.log(err);
+      handleSignupClose();
+      sweetErrorHandling(err).then();
+    }
+   }
+
+   const handleLoginRequest = async () => {
+    try {
+      const isFulfill = memberNick !== "" && memberPassword !== "";
+      if(!isFulfill) throw new Error(Messages.error3);
+      
+      const loginInput: LoginInput = {
+        memberNick: memberNick,
+        memberPassword: memberPassword,
+      };
+
+      const member = new MemberService();
+      const result = await member.login(loginInput);
+      handleLoginClose();
+      
+
+    } catch(err) {
+      console.log(err);
+      handleLoginClose();
       sweetErrorHandling(err).then();
     }
    }
@@ -183,17 +209,21 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
                 label="username"
                 variant="outlined"
                 sx={{ my: "10px" }}
+                onChange={handleUserName}
               />
               <TextField
                 id={"outlined-basic"}
                 label={"password"}
                 variant={"outlined"}
                 type={"password"}
+                onChange={handlePassword}
+                onKeyDown={handlepasswordKewDown}
               />
               <Fab
                 sx={{ marginTop: "27px", width: "120px" }}
                 variant={"extended"}
                 color={"primary"}
+                onClick={handleLoginRequest}
               >
                 <LoginIcon sx={{ mr: 1 }} />
                 Login
